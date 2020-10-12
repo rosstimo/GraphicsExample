0314b222a3db9cd3aad1ccb9f68eb64b20129439
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-12 15:39:48 -0600

    add mouse info

--- GraphicsMouseEvents/DrawWithMouseForm.vb
+++ GraphicsMouseEvents/DrawWithMouseForm.vb
@@ -1,3 +1,20 @@
 ﻿Public Class DrawWithMouseForm
+    Private Sub DrawWithMouseForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Me.Text = $"Button: {e.Button} X:{e.X}, Y:{e.Y}"
+    End Sub
 
+    Private Sub DrawWithMouseForm_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        Me.Text = $"Button: {e.Button} X:{e.X}, Y:{e.Y}"
+        Select Case e.Button
+            Case e.Button.Left
+
+            Case e.Button.Right
+
+            Case e.Button.Middle
+
+            Case Else
+
+        End Select
+
+    End Sub
 End Class
