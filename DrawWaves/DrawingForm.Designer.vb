482b36af7ac5ce3b3e87d2002ce0bac2750854da
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-22 12:24:03 -0600

    add working sin wave

--- DrawWaves/DrawingForm.Designer.vb
+++ DrawWaves/DrawingForm.Designer.vb
@@ -26,11 +26,13 @@ Partial Class DrawingForm
         Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
         Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.LocationStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SeperatorStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ColorStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DrawButton = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
@@ -59,18 +61,30 @@ Partial Class DrawingForm
         'StatusStrip1
         '
         Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LocationStatusLabel, Me.SeperatorStatusLabel, Me.ColorStatusLabel})
         Me.StatusStrip1.Location = New System.Drawing.Point(5, 557)
         Me.StatusStrip1.Name = "StatusStrip1"
         Me.StatusStrip1.Size = New System.Drawing.Size(718, 32)
         Me.StatusStrip1.TabIndex = 1
         Me.StatusStrip1.Text = "StatusStrip1"
         '
-        'ToolStripStatusLabel1
+        'LocationStatusLabel
         '
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(180, 25)
-        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        Me.LocationStatusLabel.Name = "LocationStatusLabel"
+        Me.LocationStatusLabel.Size = New System.Drawing.Size(43, 25)
+        Me.LocationStatusLabel.Text = "(x,y)"
+        '
+        'SeperatorStatusLabel
+        '
+        Me.SeperatorStatusLabel.Name = "SeperatorStatusLabel"
+        Me.SeperatorStatusLabel.Size = New System.Drawing.Size(26, 25)
+        Me.SeperatorStatusLabel.Text = " | "
+        '
+        'ColorStatusLabel
+        '
+        Me.ColorStatusLabel.Name = "ColorStatusLabel"
+        Me.ColorStatusLabel.Size = New System.Drawing.Size(55, 25)
+        Me.ColorStatusLabel.Text = "Color"
         '
         'ToolStrip1
         '
@@ -78,7 +92,7 @@ Partial Class DrawingForm
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2})
         Me.ToolStrip1.Location = New System.Drawing.Point(5, 5)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(718, 38)
+        Me.ToolStrip1.Size = New System.Drawing.Size(718, 33)
         Me.ToolStrip1.TabIndex = 2
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -88,7 +102,7 @@ Partial Class DrawingForm
         Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
         Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
         Me.ToolStripButton1.Name = "ToolStripButton1"
-        Me.ToolStripButton1.Size = New System.Drawing.Size(34, 33)
+        Me.ToolStripButton1.Size = New System.Drawing.Size(34, 28)
         Me.ToolStripButton1.Text = "ToolStripButton1"
         '
         'ToolStripButton2
@@ -100,15 +114,15 @@ Partial Class DrawingForm
         Me.ToolStripButton2.Size = New System.Drawing.Size(34, 28)
         Me.ToolStripButton2.Text = "ToolStripButton2"
         '
-        'Button1
+        'DrawButton
         '
-        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button1.Location = New System.Drawing.Point(530, 60)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(170, 75)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.DrawButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DrawButton.Location = New System.Drawing.Point(530, 60)
+        Me.DrawButton.Name = "DrawButton"
+        Me.DrawButton.Size = New System.Drawing.Size(170, 75)
+        Me.DrawButton.TabIndex = 3
+        Me.DrawButton.Text = "&Draw"
+        Me.DrawButton.UseVisualStyleBackColor = True
         '
         'Button2
         '
@@ -137,7 +151,7 @@ Partial Class DrawingForm
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(170, 75)
         Me.Button4.TabIndex = 6
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "E&xit"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'DrawingForm
@@ -148,7 +162,7 @@ Partial Class DrawingForm
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DrawButton)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.DrawingPictureBox)
@@ -169,12 +183,14 @@ Partial Class DrawingForm
     Friend WithEvents DrawingPictureBox As PictureBox
     Friend WithEvents ColorDialog1 As ColorDialog
     Friend WithEvents StatusStrip1 As StatusStrip
-    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents LocationStatusLabel As ToolStripStatusLabel
     Friend WithEvents ToolStrip1 As ToolStrip
     Friend WithEvents ToolStripButton1 As ToolStripButton
     Friend WithEvents ToolStripButton2 As ToolStripButton
-    Friend WithEvents Button1 As Button
+    Friend WithEvents DrawButton As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents ColorStatusLabel As ToolStripStatusLabel
+    Friend WithEvents SeperatorStatusLabel As ToolStripStatusLabel
 End Class
--- DrawWaves/DrawingForm.vb
+++ DrawWaves/DrawingForm.vb
@@ -1,5 +1,100 @@
-﻿Public Class DrawingForm
+﻿Option Strict On
+Option Explicit On
+Imports System.Math
+Public Class DrawingForm
+    Dim currentPosition As Point
+    Dim currentColor As Color
+
     Private Sub DrawingForm_Resize(sender As Object, e As EventArgs) Handles Me.Resize
 
+    End Sub
+
+    Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseMove
+        currentPosition = e.Location
+        LocationStatusLabel.Text = $"(X:{e.X},Y:{e.Y})"
+        ColorStatusLabel.Text = $"Color: {currentColor.Name} (A:{currentColor.A},R:{currentColor.R},G:{currentColor.G},B:{currentColor.B})"
+    End Sub
+
+    Private Sub DrawingForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        currentColor = Color.Black
+    End Sub
+
+    Sub Draw()
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
+        g.Clear(Me.BackColor)
+        g.PageUnit = GraphicsUnit.Pixel
+        g.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighSpeed
+
+        g.ScaleTransform(1, 1)
+        g.TranslateTransform(0, 0)
+        g.RotateTransform(0)
+
+    End Sub
+
+    Sub DrawLine(x1 As Integer, x2 As Integer, y1 As Integer, y2 As Integer)
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics
+        Dim pen As New Pen(currentColor)
+        g.DrawLine(pen, x1, y1, x2, y2)
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub DrawLine(ByRef x1 As Integer, ByRef y1 As Integer,
+                 ByRef x2 As Integer, ByRef y2 As Integer,
+                 ByRef sx As Single, ByRef sy As Single,
+                 ByRef dx As Single, ByRef dy As Single,
+                 ByRef R As Single)
+
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics
+        Dim pen As New Pen(currentColor)
+        g.PageUnit = GraphicsUnit.Pixel
+        g.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighSpeed
+        g.ScaleTransform(sx, sy)
+        g.TranslateTransform(dx, dy)
+        g.RotateTransform(R)
+
+
+        g.DrawLine(pen, x1, y1, x2, y2)
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub DrawSinWave()
+        'vi = vp * sin(w*t+theta)+DC
+        'w = 2 * PI * f
+        'w = 360 * f
+
+        Dim xCurrent As Double
+        Dim xMax As Single = 360
+        Dim xOld As Double
+        Dim xScale As Single = CSng(DrawingPictureBox.Width / xMax)
+        Dim xOffset As Single
+
+        Dim yCurrent As Double
+        Dim yMax As Single = 100
+        Dim yOld As Double
+        Dim yScale As Single = CSng(((DrawingPictureBox.Height - 2) / 2) / yMax)
+        Dim yOffset As Single = CSng(yMax)
+
+        Dim numberOfPoints As Integer = 180
+        Dim xPerPoints As Integer = CInt(xMax / numberOfPoints)
+
+        For currentPoint = 0 To xMax Step xPerPoints
+            xCurrent = currentPoint '
+            yCurrent = yMax * Sin((PI / 180) * xCurrent)
+            'Console.WriteLine(vi)
+
+            DrawLine(CInt(xOld), CInt(yOld), CInt(xCurrent), CInt(yCurrent), xScale, yScale, xOffset, yOffset, 0)
+
+            xOld = xCurrent
+            yOld = yCurrent
+
+        Next
+
+
+    End Sub
+
+    Private Sub DrawButton_Click(sender As Object, e As EventArgs) Handles DrawButton.Click
+        DrawSinWave()
     End Sub
 End Class
